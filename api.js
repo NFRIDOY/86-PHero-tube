2b2bd2181be865098a30600e9241640986433bfc
NF RIDOY <nfridoy@gmail.com>
2023-09-01 03:53:10 +0600

    No Data icon added

--- api.js
+++ api.js
@@ -17,14 +17,14 @@ function getTime(sec) {
     // validation
     if (sec !== undefined && sec !== NaN) {
         sec = parseInt(sec);
-        
+
         let min = parseInt(sec / 60);
-        console.log("Min",min);
+        // console.log("Min",min);
         let hour = 0;
         let day = 0;
         // let month = 0;
         if (min < 60) {
-            console.log("Min",min);
+            // console.log("Min",min);
             return (min + " Min ago");
         }
         else {
@@ -54,7 +54,7 @@ function getTime(sec) {
         }
     }
     else {
-        console.log("DONT No");
+        // console.log("DONT No");
         return ""
     }
 
@@ -89,32 +89,36 @@ const loadCategories = async () => {
 }
 loadCategories();
 const loadData = async (category_id) => {
-    const url = `https://openapi.programming-hero.com/api/videos/category/${category_id}`;
-    const res = await fetch(url)
-    const data = await res.json()
-    const dataArray = data.data;
-    // console.log(dataArray);
+    if (category_id == 1000 || category_id == 1001 || category_id == 1003) {
 
-    // DOM
-    const cardsContainer = document.getElementById("cards");
-    // Clear Before Load New Data
-    cardsContainer.innerHTML = ``;
-    for (const card of dataArray) {
-        // console.log(card.authors[0].profile_picture);
-        // console.log(card.authors[0].profile_name);
-        // console.log(card.authors[0].verified);
-        // varify(card.authors[0].verified === undefined || card.authors[0].verified === "" || card.authors[0].verified === null ? false : card.authors[0].verified );
-        // varify(false)
-        // console.log(card.others.profile_name);
-        // console.log(card.others.views);
-        console.log(card.others.posted_date);
-        // varify(card.authors[0].verified);
-        // console.log(card.authors[0].verified.lenght);
+        const url = `https://openapi.programming-hero.com/api/videos/category/${category_id}`;
+        const res = await fetch(url)
+        const data = await res.json()
+        const dataArray = data.data;
+        // console.log(dataArray);
+        // console.log(category_id);
+        // Vlid category_id: 1000  1001  1003
 
-        const badge = varify(card.authors[0].verified);
-        const time = getTime(card.others?.posted_date);
-        const div = document.createElement("div");
-        div.innerHTML = `
+        // DOM
+        const cardsContainer = document.getElementById("cards");
+        // Clear Before Load New Data
+        cardsContainer.innerHTML = ``;
+        for (const card of dataArray) {
+            // console.log(card.authors[0].profile_picture);
+            // console.log(card.authors[0].profile_name);
+            // console.log(card.authors[0].verified);
+            // varify(card.authors[0].verified === undefined || card.authors[0].verified === "" || card.authors[0].verified === null ? false : card.authors[0].verified );
+            // varify(false)
+            // console.log(card.others.profile_name);
+            // console.log(card.others.views);
+            // console.log(card.others.posted_date);
+            // varify(card.authors[0].verified);
+            // console.log(card.authors[0].verified.lenght);
+
+            const badge = varify(card.authors[0].verified);
+            const time = getTime(card.others?.posted_date);
+            const div = document.createElement("div");
+            div.innerHTML = `
         <div class="card w-76 bg-base-100 ">
                 <figure class="relative">
                     <img class="w-full h-44 rounded-lg" src="${card.thumbnail}" alt="Shoes" />
@@ -141,11 +145,30 @@ const loadData = async (category_id) => {
                 </div>
             </div>
         `;
-        // catBtn.classList.add("btn")
-        // catBtn.classList.add("mx-4")
-        // catBtn.classList.add("mx-4")
 
-        cardsContainer.appendChild(div);
+            cardsContainer.appendChild(div);
+
+        }
+
+
+    }
+    else if (category_id != 1000 || category_id != 1001 || category_id != 1003) {
+        // console.log(category_id, "data Note Found!!");
+
+        const noData = document.getElementById("noData");
+        const cardsContainer = document.getElementById("cards");
+        cardsContainer.innerHTML = ``;
+        // const div = document.createElement("div");
+        noData.innerHTML = `
+        <div class="flex flex-col items-center my-32">
+                <div>
+                    <img src="./Icon.png" alt="">
+                </div>
+                <div class="text-3xl my-10">Oops!! Sorry, There is no content here</div>
+        </div>
+        `;
+
+        // cardsContainer.appendChild(div);
 
     }
 
