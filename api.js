50bcacc0e90978b684707473ab2d564a649b5008
NF RIDOY <nfridoy@gmail.com>
2023-09-01 03:28:47 +0600

    Time added

--- api.js
+++ api.js
@@ -12,6 +12,56 @@ function varify(v) {
         return `<img id="varifyB" class=" ml-2 hidden" src="./icons/varify.svg" alt=""></img>`
     }
 }
+// Time Convertion Sec to Hours Strings
+function getTime(sec) {
+    // validation
+    if (sec !== undefined && sec !== NaN) {
+        sec = parseInt(sec);
+        
+        let min = parseInt(sec / 60);
+        console.log("Min",min);
+        let hour = 0;
+        let day = 0;
+        // let month = 0;
+        if (min < 60) {
+            console.log("Min",min);
+            return (min + " Min ago");
+        }
+        else {
+            hour = parseInt(min / 60);
+            min = min % 60;
+            // console.log(hour + " hrs", min + " min ago");
+            if (hour < 24) {
+                return (hour + " hrs " + min + " min ago");
+                // console.log(hour + " hrs", min + " min ago");
+            }
+            else {
+                day = parseInt(hour / 24);
+                hour = hour % 24;
+
+                if (day) {
+                    return (day + " Days " + hour + " hrs ", min + " min ago");
+                }
+                else {
+                    return "";
+                    return month + " Month " + day + " Days " + hour + " hrs " + min + " min ago";
+                    month = parseInt(hour / 30);
+                    day = day % 30;
+                    // console.log(month);
+                }
+
+            }
+        }
+    }
+    else {
+        console.log("DONT No");
+        return ""
+    }
+
+
+}
+
+
 const loadCategories = async () => {
     const url = "https://openapi.programming-hero.com/api/videos/categories";
     const res = await fetch(url)
@@ -21,8 +71,8 @@ const loadCategories = async () => {
 
     // DOM
     const categoriesContainer = document.getElementById("categories");
-    
-    for(const category of dataArray){
+
+    for (const category of dataArray) {
         // console.log(category.category);
         const catBtn = document.createElement("button");
         catBtn.innerHTML = `
@@ -49,7 +99,7 @@ const loadData = async (category_id) => {
     const cardsContainer = document.getElementById("cards");
     // Clear Before Load New Data
     cardsContainer.innerHTML = ``;
-    for(const card of dataArray){
+    for (const card of dataArray) {
         // console.log(card.authors[0].profile_picture);
         // console.log(card.authors[0].profile_name);
         // console.log(card.authors[0].verified);
@@ -57,16 +107,18 @@ const loadData = async (category_id) => {
         // varify(false)
         // console.log(card.others.profile_name);
         // console.log(card.others.views);
+        console.log(card.others.posted_date);
         // varify(card.authors[0].verified);
         // console.log(card.authors[0].verified.lenght);
-    
+
         const badge = varify(card.authors[0].verified);
+        const time = getTime(card.others.posted_date);
         const div = document.createElement("div");
         div.innerHTML = `
         <div class="card w-76 bg-base-100 ">
                 <figure class="relative">
                     <img class="w-full h-44 rounded-lg" src="${card.thumbnail}" alt="Shoes" />
-                    <div class="absolute bottom-3 right-3 bg-[#171717] text-white rounded-[4px] px-2 py-0">3hrs 56 min ago</div>
+                    <div class="absolute bottom-3 right-3 bg-[#171717] text-white rounded-[4px] px-2 py-0">${time}</div>
                 </figure>
                 <div class="card-body flex flex-row items-start ml-0 relative -left-7">
                     <div class="avatar">
