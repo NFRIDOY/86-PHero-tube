75ca101e9b7daf76a74784e1a96e139d314e888d
NF RIDOY <nfridoy@gmail.com>
2023-08-31 23:36:45 +0600

    card API load

--- api.js
+++ api.js
@@ -2,13 +2,17 @@ const loadCategories = async () => {
     const url = "https://openapi.programming-hero.com/api/videos/categories";
     const res = await fetch(url)
     const data = await res.json()
-    // console.log(data.data[1].category);
+    const dataArray = data.data;
+    // console.log(data.data);
+
+    // DOM
     const categoriesContainer = document.getElementById("categories");
-    for(const category of data.data){
-        console.log(category.category);
+    
+    for(const category of dataArray){
+        // console.log(category.category);
         const catBtn = document.createElement("button");
         catBtn.innerHTML = `
-        <button class="btn mx-4 active:bg-[#FF1F3D] text-black hover:bg-black hover:text-white">${category.category}</button>
+        <button onclick="loadData(${category.category_id})" class="btn mx-4 active:bg-[#FF1F3D] text-black hover:bg-black hover:text-white">${category.category}</button>
         `;
         // catBtn.classList.add("btn")
         // catBtn.classList.add("mx-4")
@@ -20,6 +24,29 @@ const loadCategories = async () => {
 
 }
 loadCategories();
-function displyCategories() {
+const loadData = async (category_id) => {
+    const url = `https://openapi.programming-hero.com/api/videos/category/${category_id}`;
+    const res = await fetch(url)
+    const data = await res.json()
+    const dataArray = data.data;
+    console.log(dataArray);
+
+    // DOM
+    const cardsContainer = document.getElementById("cards");
+    cardsContainer.innerHTML = ``;
+    for(const card of dataArray){
+        // console.log(category.category);
+        const div = document.createElement("div");
+        div.innerHTML = `
+        title: ${card.title}
+        `;
+        // catBtn.classList.add("btn")
+        // catBtn.classList.add("mx-4")
+        // catBtn.classList.add("mx-4")
+
+        cardsContainer.appendChild(div);
+
+    }
 
 }
+loadData();
